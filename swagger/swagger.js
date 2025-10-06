755ca27f73d3fd7362b312e4c360fa7f56bf479a
KimJeongChul <jhtpo1230@skuniv.ac.kr>
2025-10-07 00:09:43 +0900

    swagger, todoAPI yaml 추가 커밋

---
+++ swagger/swagger.js
@@ -0,0 +1,23 @@
+require('dotenv').config({ path: '../.env' });
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'TodoList API',
+            version: '1.0.0',
+            description: 'Todo CRUD API 문서'
+        },
+        servers: [
+            { url: `http://localhost:${process.env.PORT}` }
+        ]
+    },
+    apis: ['./swagger-docs/*.yaml'],
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = { swaggerUi, specs };
+console.log('✅ Swagger spec loaded:', Object.keys(specs));
