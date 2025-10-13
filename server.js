5db9002ddebd33281967cdaac1518437c243dd94
KimJeongChul <jhtpo1230@skuniv.ac.kr>
2025-10-13 15:04:14 +0900

    CORS 설정 커밋

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const todoRoutes = require('./routes/todo-route');
 const { swaggerUi, specs } = require('./swagger/swagger');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({origin : 'http://localhost:3000'}));
 
 // 라우트 등록
 app.use('/todo', todoRoutes);
