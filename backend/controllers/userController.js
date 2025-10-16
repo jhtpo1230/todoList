c4362ce13c8052e465d92929afca02e0b691c70c
KimJeongChul <jhtpo1230@skuniv.ac.kr>
2025-10-17 02:28:39 +0900

    회원가입, 로그인에 서버 오류(500) status 코드 추가

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,54 +2,68 @@ const pool = require('../db/todoListDB');
 
 // 회원가입 API : POST
 exports.joinUser = async (req, res) => {
-    const { login_id, password } = req.body;
-    const [checkLoginIdExist] = await pool.query(
-        'SELECT * FROM user WHERE login_id = ?', login_id
-    );
-    if (checkLoginIdExist.length > 0) {
-        return res.status(400).json({
-            JoinSuccess: false,
-            message: "이미 존재하는 ID 입니다."
-        });
-    } else {
-        await pool.query(
-            'INSERT INTO user (login_id, password) VALUES (?, ?)',
-            [login_id, password]
+    try {
+        const { login_id, password } = req.body;
+        const [checkLoginIdExist] = await pool.query(
+            'SELECT * FROM user WHERE login_id = ?', login_id
         );
+        if (checkLoginIdExist.length > 0) {
+            return res.status(400).json({
+                JoinSuccess: false,
+                message: "이미 존재하는 ID 입니다."
+            });
+        } else {
+            await pool.query(
+                'INSERT INTO user (login_id, password) VALUES (?, ?)',
+                [login_id, password]
+            );
 
-        res.status(201).json({
-            JoinSuccess: true,
-            message: `${login_id} 님 회원가입을 축하드립니다.`
+            res.status(201).json({
+                JoinSuccess: true,
+                message: `${login_id} 님 회원가입을 축하드립니다.`
+            })
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: "서버 내부에 오류 발생"
         })
     }
 }
 
 // 로그인 API : POST
 exports.loginUser = async (req, res) => {
-    const { login_id, password } = req.body;
-    const [checkLoginIdExist] = await pool.query(
-        'SELECT * FROM user WHERE login_id = ?', login_id
-    );
-    if (checkLoginIdExist.length === 0) {
-        return res.status(400).json({
-            loginSuccess: false,
-            message: "존재하지 않는 ID 입니다."
-        });
-    } else {
-        const [userInfo] = await pool.query(
-            'SELECT login_id, password FROM user WHERE login_id=?', login_id
+    try {
+        const { login_id, password } = req.body;
+        const [checkLoginIdExist] = await pool.query(
+            'SELECT * FROM user WHERE login_id = ?', login_id
         );
-        if (userInfo[0].password !== password) {
-            res.status(400).json({
-                pwSuccess: false,
-                message: `비밀번호가 틀렸습니다.`
-            })
+        if (checkLoginIdExist.length === 0) {
+            return res.status(400).json({
+                loginSuccess: false,
+                message: "존재하지 않는 ID 입니다."
+            });
         } else {
-            res.status(200).json({
-                JoinSuccess: true,
-                pwSuccess: true,
-                message: `${login_id} 님 환영합니다!`
-            })
+            const [userInfo] = await pool.query(
+                'SELECT login_id, password FROM user WHERE login_id=?', login_id
+            );
+            if (userInfo[0].password !== password) {
+                res.status(400).json({
+                    pwSuccess: false,
+                    message: `비밀번호가 틀렸습니다.`
+                })
+            } else {
+                res.status(200).json({
+                    JoinSuccess: true,
+                    pwSuccess: true,
+                    message: `${login_id} 님 환영합니다!`
+                })
+            }
         }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: "서버 내부에 오류 발생"
+        })
     }
 }
