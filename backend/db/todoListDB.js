72173f542475faccb1659f9ea53d9bb4502bfc2f
KimJeongChul <jhtpo1230@skuniv.ac.kr>
2025-10-22 11:33:19 +0900

    db hoet, port 수정

--- backend/db/todoListDB.js
+++ backend/db/todoListDB.js
@@ -3,11 +3,14 @@ const todoListDB = require('mysql2/promise');
 
 const isVercel = process.env.NODE_ENV === 'production';
 const pool = todoListDB.createPool({
-  host: isVercel ? process.env.DB_HOST_CT : process.env.DB_HOST_LOCAL,
-  port: isVercel ? process.env.DB_PORT_CT : process.env.DB_PORT_LOCAL,
+  host: isVercel ? process.env.DB_HOST : process.env.DB_HOST_LOCAL,
+  port: isVercel ? process.env.DB_PORT : process.env.DB_PORT_LOCAL,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
 
 module.exports = pool;
+console.log('✅ NODE_ENV:', process.env.NODE_ENV);
+console.log('✅ DB_HOST_CT:', process.env.DB_HOST_CT);
+console.log('✅ DB_PORT_CT:', process.env.DB_PORT_CT);
